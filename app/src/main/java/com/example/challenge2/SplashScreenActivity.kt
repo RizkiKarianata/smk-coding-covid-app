1373ca547406b904e36face6f65536f96ecdf513
RizkiKarianata <natarizky884@gmail.com>
2020-05-11 16:06:27 +0700

    Add Function Splash Screen

--- app/src/main/java/com/example/challenge2/SplashScreenActivity.kt
+++ app/src/main/java/com/example/challenge2/SplashScreenActivity.kt
@@ -1,5 +1,6 @@
 package com.example.challenge2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,19 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(5000)
+
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
     }
 }
