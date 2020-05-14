bbc41c5d41c13b17e658e9d287d89265035a5703
RizkiKarianata <natarizky884@gmail.com>
2020-05-14 11:27:07 +0700

    Add Fragment

--- app/src/main/java/com/example/challenge2/MainActivity.kt
+++ app/src/main/java/com/example/challenge2/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.challenge2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
     }
 }
