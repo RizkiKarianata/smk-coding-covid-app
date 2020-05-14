e6c5b4653a2fea3d32b884b728c0a05ca40bf905
RizkiKarianata <natarizky884@gmail.com>
2020-05-14 11:17:05 +0700

    Delete Status Bar

--- app/src/main/java/com/example/challenge2/SplashScreenActivity.kt
+++ app/src/main/java/com/example/challenge2/SplashScreenActivity.kt
@@ -3,11 +3,15 @@ package com.example.challenge2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash_screen)
 
         val background = object : Thread() {
