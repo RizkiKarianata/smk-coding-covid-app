8d837115e6bd755ef22c1174c00b4373608a115f
RizkiKarianata <natarizky884@gmail.com>
2020-05-14 12:15:08 +0700

    Edit Profile Fragment

--- app/src/main/java/com/example/challenge2/ProfileFragment.kt
+++ app/src/main/java/com/example/challenge2/ProfileFragment.kt
@@ -5,18 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.Nullable
 
-/**
- * A simple [Fragment] subclass.
- */
 class ProfileFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onViewCreated(
+        view: View,
+        @Nullable savedInstanceState: Bundle?
+    ) {
+        super .onViewCreated(view, savedInstanceState)
     }
 
 }
