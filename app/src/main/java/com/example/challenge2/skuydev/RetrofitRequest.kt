dc94a65676f8c6b3aef37c7ccf126097e0ea904b
RizkiKarianata <natarizky884@gmail.com>
2020-05-16 16:54:33 +0700

    Delete Home Fragment

---
+++ app/src/main/java/com/example/challenge2/skuydev/RetrofitRequest.kt
@@ -0,0 +1,27 @@
+package com.example.challenge2.skuydev
+
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+fun httpClient(): OkHttpClient {
+    val logInterceptor = HttpLoggingInterceptor()
+    logInterceptor.level = HttpLoggingInterceptor.Level.BODY
+    val builder = OkHttpClient.Builder()
+    builder.readTimeout(15, TimeUnit.SECONDS )
+    builder.connectTimeout(15, TimeUnit.SECONDS )
+    builder.addInterceptor(logInterceptor)
+    return builder.build()
+}
+inline fun <reified T> apiRequest(okHttpClient: OkHttpClient): T {
+    val gson = GsonBuilder().create()
+    val retrofit = Retrofit.Builder()
+        .baseUrl( "https://api.covid19.skuy.dev/v1/" )
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+    return retrofit.create(T::class.java)
+}
