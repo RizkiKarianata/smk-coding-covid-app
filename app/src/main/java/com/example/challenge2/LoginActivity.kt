04d1c5c0246765c1d88b2654bb771972be7720cf
RizkiKarianata <natarizky884@gmail.com>
2020-05-17 23:29:47 +0700

    Add Login Activity

--- app/src/main/java/com/example/challenge2/LoginActivity.kt
+++ app/src/main/java/com/example/challenge2/LoginActivity.kt
@@ -1,10 +1,14 @@
 package com.example.challenge2
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.net.Uri
+import android.widget.Toast
+import android.view.View
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -14,8 +18,14 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_login)
+        getData()
         btnSkip.setOnClickListener { navigationToMainActivity() }
         btnSignUp.setOnClickListener { navigationToRegisterActivity() }
+    }
+    private fun getData() {
+        val bundle = intent.extras
+        val username = bundle?.getString("username")
+        val password = bundle?.getString("password")
     }
     private fun navigationToMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
