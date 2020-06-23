f58c5551a93c3bc6fa3451470781222c9642ab76
RizkiKarianata <natarizky884@gmail.com>
2020-06-23 11:09:12 +0700

    Add Activity Login

---
+++ app/src/main/java/com/example/challenge2/Activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.challenge2.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.challenge2.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/example/challenge2/Activity/SplashScreenActivity.kt
+++ app/src/main/java/com/example/challenge2/Activity/SplashScreenActivity.kt
@@ -1,13 +1,33 @@
 package com.example.challenge2.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.challenge2.R
+import android.view.Window
+import android.view.WindowManager
+import com.example.challenge2.MainActivity
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash_screen)
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(5000)
+
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
     }
 }
