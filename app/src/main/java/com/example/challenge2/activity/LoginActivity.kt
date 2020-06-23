a516588755e18f3db41bd6c0b841caac16b5e769
RizkiKarianata <natarizky884@gmail.com>
2020-06-23 13:36:20 +0700

    Update Login

--- app/src/main/java/com/example/challenge2/activity/LoginActivity.kt
+++ app/src/main/java/com/example/challenge2/activity/LoginActivity.kt
@@ -6,13 +6,51 @@ import android.os.Bundle
 import com.example.challenge2.R
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
+import com.example.challenge2.MainActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private var usernameInput : String = ""
+    private var passwordInput : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_login)
+        btnLogin.setOnClickListener { validationInput() }
+        btnRegister.setOnClickListener { navigationToRegisterActivity() }
+    }
+    private fun navigationToRegisterActivity() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+    private fun dataRegister() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+    private fun validationInput(){
+        val bundle = intent.extras
+        usernameInput = usernameLog.text.toString()
+        passwordInput = passwordLog.text.toString()
+        when {
+            usernameInput.isEmpty() -> usernameLog.error = "Nama Pengguna harus diisi"
+            passwordInput.isEmpty() -> passwordLog.error = "Kata Sandi harus diisi"
+            else -> {
+                val datausername = bundle?.getString("username")
+                val datapassword = bundle?.getString("password")
+                if(usernameInput == datausername && passwordInput == datapassword) {
+                    tampilToast("Berhasil masuk")
+                    dataRegister()
+                }else {
+                    tampilToast("Gagal masuk")
+                }
+            }
+        }
+    }
+    private fun tampilToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/challenge2/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/challenge2/activity/RegisterActivity.kt
@@ -6,13 +6,63 @@ import android.os.Bundle
 import com.example.challenge2.R
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    private var nameInput : String = ""
+    private var usernameInput : String = ""
+    private var emailInput : String = ""
+    private var telephoneInput : String = ""
+    private var addressInput : String = ""
+    private var passwordInput : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_register)
+        btnRegisterCovid.setOnClickListener { validationInput() }
+        btnLoginCovid.setOnClickListener { navigationToLoginActivity() }
+    }
+    private fun navigationToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+    private fun validationInput(){
+        nameInput = nameReg.text.toString()
+        usernameInput = usernameReg.text.toString()
+        emailInput = emailReg.text.toString()
+        telephoneInput = telephoneReg.text.toString()
+        addressInput = addressReg.text.toString()
+        passwordInput = passwordReg.text.toString()
+        when {
+            nameInput.isEmpty() -> nameReg.error = "Nama Lengkap harus diisi"
+            usernameInput.isEmpty() -> usernameReg.error = "Nama Pengguna harus diisi"
+            emailInput.isEmpty() -> emailReg.error = "Alamat Email harus diisi"
+            telephoneInput.isEmpty() -> telephoneReg.error = "Nomor Telepon harus diisi"
+            addressInput.isEmpty() -> addressReg.error = "Alamat harus diisi"
+            passwordInput.isEmpty() -> passwordReg.error = "Kata Sandi harus diisi"
+            else -> {
+                tampilToast("Berhasil mendaftar")
+                goToLoginActivity()
+            }
+        }
+    }
+    private fun tampilToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    private fun goToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        val bundle = Bundle()
+        bundle.putString("name", nameInput)
+        bundle.putString("username", usernameInput)
+        bundle.putString("email", emailInput)
+        bundle.putString("telephone", telephoneInput)
+        bundle.putString("address", addressInput)
+        bundle.putString("password", passwordInput)
+        intent.putExtras(bundle)
+        startActivity(intent)
     }
 }
